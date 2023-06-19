4170b2cfe47671721334c92a6093457d3d8e6aab
Name <email@exaple.ru>
2023-06-19 08:35:37 +0300

    8.1.7

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -13,6 +13,10 @@ namespace FileSystem
         static void Main(string[] args)
         {
             FileSystem fileSystem = new FileSystem("C", 500000, 366000);
+            Folder folder = new Folder("aboba");
+            folder.AddFile("Bobik");
+            folder.AddFile("Tolik");
+            folder.ShowList();
         }
 
         class FileSystem
@@ -20,21 +24,45 @@ namespace FileSystem
             public string _name { get; }
             public double _volume { get; }
             public double _freVolume { get; }
+
             public FileSystem(string name, double volume, double freVolume)
             {
                 _name = name;
                 _volume = volume;
                 _freVolume = freVolume;
             }
-            Dictionary<string,Folder> newFolder = new Dictionary<string, Folder>();
+            Dictionary<string, Folder> newFolder = new Dictionary<string, Folder>();
+
             public void CreateFolder(string nameFolder)
             {
-                newFolder.Add(nameFolder,new Folder());
+                newFolder.Add(nameFolder, new Folder(null));
             }
         }
         public class Folder
         {
-            List<string> files { get; set; }=new List<string>();
+            public string _name { get; set; }
+            List<string> files { get; set; } = new List<string>();
+
+            public Folder(string name)
+            {
+                _name = name;
+            }
+
+            public void AddFile(string name)
+            {
+                if (!files.Contains(name))
+                {
+                    files.Add(name);
+                }
+            }
+
+            public void ShowList()
+            {
+                foreach(string file in files) 
+                {
+                    Console.WriteLine(file+"\n");
+                }
+            }
         }
     }
 }
